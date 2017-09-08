6ad4ac23a33e669a465fd97d512e778258ed63d2
newpolaris <newpolaris@gmail.com>
2017-09-08 22:44:31 +0900

    basic file parser

--- Shader/Boost.Spirit.X3/Boost.Spirit.X3/main.cpp
+++ Shader/Boost.Spirit.X3/Boost.Spirit.X3/main.cpp
@@ -17,24 +17,29 @@
 #include <boost/fusion/include/io.hpp>
 #pragma warning(pop)
 
+namespace client {
+namespace fusion = boost::fusion;
+namespace x3 = boost::spirit::x3;
+
 namespace ast 
 {
-    namespace fusion = boost::fusion;
-    namespace x3 = boost::spirit::x3;
-
     struct sampler_desc;
     struct blend_desc;
+    struct depth_stencil_desc;
+    struct rasterizer_desc;
     struct statement : x3::variant<
           x3::forward_ast<sampler_desc>
         , x3::forward_ast<blend_desc>
+        , x3::forward_ast<depth_stencil_desc>
+        , x3::forward_ast<rasterizer_desc>
     >
     {
         using base_type::base_type;
         using base_type::operator=;
     };
 
-    typedef std::map<std::string, std::string> sampler_map;
     typedef std::pair<std::string, std::string> sampler_key_value;
+    typedef std::map<std::string, std::string> sampler_map;
     struct sampler_desc
     {
         std::string name;
@@ -42,7 +47,7 @@ namespace ast
         sampler_map entries;
     };
 
-    typedef std::pair<uint32_t, std::string> blend_key;
+    typedef std::pair<std::string, uint32_t> blend_key;
     typedef std::pair<blend_key, std::string> blend_key_value;
     typedef std::map<blend_key, std::string> blend_map;
     struct blend_desc
@@ -51,31 +56,58 @@ namespace ast
         blend_map entries;
     };
 
+    typedef std::pair<std::string, std::string> depth_key_value;
+    typedef std::map<std::string, std::string> depth_map;
+    struct depth_stencil_desc
+    {
+        std::string name;
+        depth_map entries;
+    };
+
+    typedef std::pair<std::string, std::string> raster_key_value;
+    typedef std::map<std::string, std::string> raster_map;
+    struct rasterizer_desc
+    {
+        std::string name;
+        raster_map entries;
+    };
+
     struct program
     {
         std::list<statement> states;
     };
 }
+}
 
 // We need to tell fusion about our rexpr struct
 // to make it a first-class fusion citizen
 BOOST_FUSION_ADAPT_STRUCT(
-    ast::sampler_desc,
+    client::ast::sampler_desc,
     (std::string, name)
     (uint32_t, slot)
-    (ast::sampler_map, entries)
+    (client::ast::sampler_map, entries)
 )
 BOOST_FUSION_ADAPT_STRUCT(
-    ast::blend_desc,
+    client::ast::blend_desc,
     (std::string, name)
-    (ast::blend_map, entries)
+    (client::ast::blend_map, entries)
 )
 BOOST_FUSION_ADAPT_STRUCT(
-    ast::program,
-    (std::list<ast::statement>, states)
+    client::ast::depth_stencil_desc,
+    (std::string, name)
+    (client::ast::depth_map, entries)
+)
+BOOST_FUSION_ADAPT_STRUCT(
+    client::ast::rasterizer_desc,
+    (std::string, name)
+    (client::ast::raster_map, entries)
+)
+BOOST_FUSION_ADAPT_STRUCT(
+    client::ast::program,
+    (std::list<client::ast::statement>, states)
 )
 
-namespace parser {
+namespace client { namespace parser {
     namespace x3 = boost::spirit::x3;
     namespace ascii = boost::spirit::x3::ascii;
 
@@ -126,11 +158,11 @@ namespace parser {
         ;
     auto const slot = 
            x3::lit(":") 
-        >> x3::lit("register") 
-        >> x3::lit("(") 
+        >> "register"
+        >> "("
         >> x3::omit[x3::alpha]
         >> x3::uint32 
-        >> x3::lit(")");
+        >> ")";
 
     x3::rule<class sampler_state, ast::sampler_desc> const 
         sampler_state = "sampler_state";
@@ -139,7 +171,7 @@ namespace parser {
     auto const sampler_key_value_def =
         lexeme[+(char_ - x3::space)] >> '=' >> lexeme[+(char_ - x3::space - ';')] >> lit(";");
     auto const sampler_state_def = 
-           x3::lit("SamplerState")
+           "SamplerState"
         >> identifier
         >> slot
         >> '{' 
@@ -150,42 +182,79 @@ namespace parser {
 
     x3::rule<class blend_state, ast::blend_desc> const 
         blend_state = "blend_state";
+    x3::rule<class blend_key, ast::blend_key> const
+        blend_key = "blend_key";
     x3::rule<class blend_key_value, ast::blend_key_value> const
         blend_key_value = "blend_key_value";
-    auto const blend_key_value_def =
+    auto const blend_key_def =
            lexeme[+(x3::alpha - '[')]
-        >> x3::lit('[')
+        >> '['
         >> x3::uint32
-        >> x3::lit(']')
-        >> x3::lit('=')
+        >> ']'
+        ;
+    auto const blend_key_value_def =
+           blend_key
+        >> '='
         >> lexeme[+(x3::alpha | '_')]
-        >> x3::lit(';')
+        >> ';'
         ;
     auto const blend_state_def =
-           x3::lit("BlendState")
+           "BlendState"
         >> identifier
-        >> x3::lit('{')
+        >> '{'
         >> *blend_key_value
-        >> x3::lit('}')
-        >> x3::lit(';')
+        >> '}'
+        >> ';'
         ;
 
+    x3::rule<class depth_stencil_state, ast::depth_stencil_desc> const 
+        depth_stencil_state = "depth_stencil_state";
+    x3::rule<class depth_key_value, ast::depth_key_value> const
+        depth_key_value = "depth_key_value";
+    auto const depth_key_value_def =
+        lexeme[+(char_ - x3::space)] >> '=' >> lexeme[+(char_ - x3::space - ';')] >> lit(";");
+    auto const depth_stencil_state_def = 
+           "DepthStencilState"
+        >> identifier
+        >> '{' 
+        >> *depth_key_value 
+        >> '}'
+        >> ";"
+        ;
+
+    x3::rule<class rasterizer_state, ast::rasterizer_desc> const 
+        rasterizer_state = "rasterizer_state";
+    x3::rule<class raster_key_value, ast::raster_key_value> const
+        raster_key_value = "raster_key_value";
+    auto const raster_key_value_def =
+        lexeme[+(char_ - x3::space)] >> '=' >> lexeme[+(char_ - x3::space - ';')] >> lit(";");
+    auto const rasterizer_state_def = 
+           "RasterizerState"
+        >> identifier
+        >> '{' 
+        >> *raster_key_value
+        >> '}'
+        >> ";"
+        ;
+    
     x3::rule<class statement, ast::statement> const 
         statement = "statement";
     auto const statement_def = 
         (
               blend_state
             | sampler_state
+            | depth_stencil_state
         );
     x3::rule<class program, ast::program> const 
         program = "program";
-    auto const program_def = *statement;
+    auto const program_def = +statement;
 
     BOOST_SPIRIT_DEFINE(
         sampler_state, sampler_key_value,
-        blend_state, blend_key_value,
-        // program,
-        // statement,
+        blend_state, blend_key, blend_key_value,
+        depth_stencil_state, depth_key_value,
+        program,
+        statement,
         block);
 
     struct expression_class
@@ -206,6 +275,7 @@ namespace parser {
             return x3::error_handler_result::fail;
         }
     };
+}
 }
 
 std::string simpleRead( const std::string& FileName )
@@ -236,25 +306,17 @@ std::wstring readFileEnc( std::wstring FileName, std::string Enc )
 }
 
 int main() {
-    // auto&& k = readFile( "pmx.fx" );
-    // auto&& k3 = readFileEnc( L"pmx.fx", "utf-8" );
-    // auto&& k2 = readFileEnc( L"mikudayo-3_6.fx", "shift-jis" );
     auto&& k1 = simpleRead( "pmx.fx" );
+    // auto&& k0 = readFile( "pmx.fx" );
+    // auto&& k2 = readFileEnc( L"mikudayo-3_6.fx", "shift-jis" );
+    // auto&& k3 = readFileEnc( L"pmx.fx", "utf-8" );
     std::string::const_iterator iter = k1.begin();
     std::string::const_iterator end = k1.end();
     namespace x3 = boost::spirit::x3;
+    using namespace client;
 
-    ast::sampler_desc desc;
-    if (x3::phrase_parse(iter, end, parser::sampler_state, parser::skip, desc))
-        std::cout << "parse sucess:\n";
-#if 0
     ast::program program;
     while (x3::phrase_parse(iter, end, parser::program, parser::skip, program))
         std::cout << "parse sucess:\n";
-#else
-    ast::blend_desc blend_desc;
-    while (x3::phrase_parse(iter, end, parser::blend_state, parser::skip, blend_desc))
-        std::cout << "parse sucess:\n";
-#endif
     return 0;
 }
